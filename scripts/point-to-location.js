d6795864795cf056acebafa7703426cf69bd8f2f
Wim Van Aerschot <>
2021-12-03 16:43:40 +0100

    add documentation

--- scripts/point-to-location.js
+++ scripts/point-to-location.js
@@ -1,9 +1,23 @@
 /*
 
-Maak een image aan met een pijl die naar boven wijst.
+Gebruik:
 
+Maak een image aan met een pijl die naar boven wijst.
 Op computer zal kompas altijd het noorden aanwijzen, dus rekeninghoudend met een normale kaart-oriëntatie.
 Op telefoon zal deze rekening houden met het kompas van de telefoon zelf.
+
+Je zal ook een bericht moeten verzien dat je kan tonen/verbergen om permissies op te vragen. Dit gaat enkel nodig zijn voor iOS,
+dus dat bericht moet by default verborgen zijn. (vandaag dat het voorbeeld al meteen onHideRequestPermissions() oproept ook)
+
+Eerste argument: lat van gps
+Tweede argument: lon van gps
+Derde argument: lat van gewenste locatie
+Vierde argument: lon van gewenste locatie
+Vijfde argument: selector voor de image van de pijl
+Zesde argument: selector voor de button om permissie te geven
+Zevende argument: de onShow functie voor de permissies
+Achtste argument: de onHide functie voor de permissies
+
 
 //
 // .html
@@ -17,13 +31,27 @@ Op telefoon zal deze rekening houden met het kompas van de telefoon zelf.
 // body:
 
 <img src="path/to/arrow.svg" alt="volg mij" id="point-to-location"/>
+<div id="request-permissions">
+  Mogen we je kompas gebruiken om je naar de juiste locatie te gidsen?
+  <button id="request-permissions-button">Geef goedkeuring</button>
+</div>
 
 //
 // .js
 //
 
+const requestPermissionsElement = document.querySelector('#request-permissions')
+function onShowRequestPermissions() {
+  requestPermissionsElement.style.display = 'block';
+}
+
+function onHideRequestPermissions() {
+  requestPermissionsElement.style.display = 'none';
+}
+onHideRequestPermissions();
+
 function success(postion) {
-  pointToLocation(position.coords.latitude, position.coords.longitude, coordinates.latitude, coordinates.longitude, '#point-to-location');
+  pointToLocation(position.coords.latitude, position.coords.longitude, coordinates.latitude, coordinates.longitude, '#point-to-location', '#request-permissions-button', onShowRequestPermissions, onHideRequestPermissions);
 }
 
 ...
