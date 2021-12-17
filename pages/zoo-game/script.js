ce4ed6f16de279194ce3d98b4f1240a08ba8a015
Wim Van Aerschot <>
2021-12-17 22:02:23 +0100

    added gorillascream

--- pages/zoo-game/script.js
+++ pages/zoo-game/script.js
@@ -1,12 +1,31 @@
+const countDownElement = document.querySelector('#countdown');
+const countDown3Element = document.querySelector('#countdown-3');
+const countDown2Element = document.querySelector('#countdown-2');
+const countDown1Element = document.querySelector('#countdown-1');
+const volumeElement = document.querySelector('#volume-meter');
+const successElement = document.querySelector('#volume-success');
+const failedElement = document.querySelector('#volume-failed');
+
+/* microphone */
 const audioVolumeArray = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 let now = new Date().getTime();
+let microphoneReady = false;
+let maxVolume = 0;
 
 function microphoneSuccess(volume) {
+  if (!microphoneReady) {
+    startCountdown();
+    microphoneReady = true;
+  }
   const newNow = new Date().getTime();
   if (newNow > now + 20) {
     now = newNow;
     audioVolumeArray.unshift(Math.round(volume));
     audioVolumeArray.pop();
+
+    if (volume > maxVolume) {
+      maxVolume = volume;
+    }
 
     drawVolume();
   }
@@ -17,24 +36,59 @@ getMicrophone(microphoneSuccess);
 function drawVolume() {
   audioVolumeArray.forEach((volume, i) => {
     if (i === 0) {
-      document.querySelector(`#vol-0`).style.transform = `scale(1, ${
-        volume / 100
-      })`;
+      document.querySelector(`#vol-0`).style.height =
+        (71 * volume) / 100 + 5 + 'px';
     } else {
-      document.querySelector(`#vol-l${i}`).style.transform = `scale(1, ${
-        volume / 100
-      })`;
-      document.querySelector(`#vol-r${i}`).style.transform = `scale(1, ${
-        volume / 100
-      })`;
+      document.querySelector(`#vol-l${i}`).style.height =
+        (71 * volume) / 100 + 5 + 'px';
+      document.querySelector(`#vol-r${i}`).style.height =
+        (71 * volume) / 100 + 5 + 'px';
     }
   });
 }
 
 drawVolume();
 
-const continueButton = document.querySelector('#continue-button');
+/* countDown */
+function startCountdown() {
+  successElement.style.display = 'none';
+  failedElement.style.display = 'none';
+  volumeElement.style.display = 'none';
+  countDownElement.style.display = 'flex';
 
-continueButton.onclick = () => {
-  location.assign(`../zoo-continue/index.html`);
-};
+  countDown3Element.style.display = 'block';
+  countDown2Element.style.display = 'none';
+  countDown1Element.style.display = 'none';
+
+  setTimeout(() => {
+    countDown3Element.style.display = 'none';
+    countDown2Element.style.display = 'block';
+    countDown1Element.style.display = 'none';
+  }, 1000);
+  setTimeout(() => {
+    countDown3Element.style.display = 'none';
+    countDown2Element.style.display = 'none';
+    countDown1Element.style.display = 'block';
+  }, 2000);
+  setTimeout(() => {
+    countDown3Element.style.display = 'none';
+    countDown2Element.style.display = 'none';
+    countDown1Element.style.display = 'none';
+    volumeElement.style.display = 'flex';
+    countDownElement.style.display = 'none';
+    maxVolume = 0;
+  }, 3000);
+  setTimeout(() => {
+    if (maxVolume > 55) {
+      volumeElement.style.display = 'none';
+      successElement.style.display = 'flex';
+      setTimeout(() => {
+        location.assign(`../zoo-continue/index.html`);
+      }, 1000);
+    } else {
+      volumeElement.style.display = 'none';
+      failedElement.style.display = 'flex';
+      setTimeout(startCountdown, 1000);
+    }
+  }, 6000);
+}
