2e251aeb9a8948e66edd4e5f4ba3b3784ae9b32d
Wim Van Aerschot <>
2021-12-17 13:20:27 +0100

    added start page & text speech design

--- pages/start/script.js
+++ pages/start/script.js
@@ -1,6 +1,30 @@
-const startButton = document.querySelector('#start-button');
+const AMOUNT_OF_SPEECH = 3;
 
-startButton.onclick = () => {
+function setVisibleSpeech(index) {
+  document.querySelectorAll('.speech').forEach((el) => {
+    el.style.display = 'none';
+  });
+  document.querySelector(`#speech-${index}`).style.display = 'block';
+}
+
+for (let index = 1; index <= AMOUNT_OF_SPEECH; index++) {
+  const next = document.querySelector(`#speech-${index} .speech-button-next`);
+  const prev = document.querySelector(`#speech-${index} .speech-button-prev`);
+
+  if (next) {
+    next.onclick = () => {
+      setVisibleSpeech(index + 1);
+    };
+  }
+
+  if (prev) {
+    prev.onclick = () => {
+      setVisibleSpeech(index - 1);
+    };
+  }
+}
+
+document.querySelector('.speech-button-ok').onclick = () => {
   const coordinates = '51.217509,4.422590';
   const locationName = 'Zoo';
   const nextPage = 'zoo';
