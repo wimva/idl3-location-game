284b31609f4545c6123e5895549ce046da3622f6
Kim Matthé <kim@monsieurmoire.com>
2021-12-19 23:24:44 +0100

    kim additions

---
+++ pages/sluisstraat/script.js
@@ -0,0 +1,36 @@
+const audioPlayer = document.getElementById('cargo');
+  
+
+const AMOUNT_OF_SPEECH = 3;
+
+function setVisibleSpeech(index) {
+  document.querySelectorAll('.speech').forEach((el) => {
+    el.style.display = 'none';
+  });
+  //if(bananaFound>=bananaMax) {
+    document.querySelector(`#speech-${index}`).style.display = 'block';
+  //}
+  
+}
+
+
+for (let index = 1; index <= AMOUNT_OF_SPEECH; index++) {
+  const next = document.querySelector(`#speech-${index} .speech-button-next`);
+  const prev = document.querySelector(`#speech-${index} .speech-button-prev`);
+
+  if (next) {
+    next.onclick = () => {
+      setVisibleSpeech(index + 1);
+      if(index>=2) {
+        audioPlayer.play();
+        $('#fin').delay(5500).fadeIn(4000);
+      }
+    };
+  }
+
+  if (prev) {
+    prev.onclick = () => {
+      setVisibleSpeech(index - 1);
+    };
+  }
+}
--- pages/waagnatie/script.js
+++ pages/waagnatie/script.js
@@ -1,1 +1,43 @@
-// endgame
+const AMOUNT_OF_SPEECH = 7;
+
+function setVisibleSpeech(index) {
+  document.querySelectorAll('.speech').forEach((el) => {
+    el.style.display = 'none';
+  });
+  document.querySelector(`#speech-${index}`).style.display = 'block';
+}
+
+for (let index = 1; index <= AMOUNT_OF_SPEECH; index++) {
+  const next = document.querySelector(`#speech-${index} .speech-button-next`);
+  const prev = document.querySelector(`#speech-${index} .speech-button-prev`);
+
+  if (next) {
+    next.onclick = () => {
+      setVisibleSpeech(index + 1);
+    };
+  }
+
+  if (prev) {
+    prev.onclick = () => {
+      setVisibleSpeech(index - 1);
+    };
+  }
+}
+
+document.querySelector(`#ok-button`).onclick = () => {
+    let secretCode = $(`#riddle`).val().toLowerCase();
+    if(secretCode==="3a9g") {
+        setVisibleSpeech(6);
+    } else {
+        alert("VERKEERDE CODE!\nProbeer opnieuw");
+    }
+};
+
+document.querySelector('.continue-button').onclick = () => {
+    const coordinates = '51.23570256063406,4.405535306808561';
+    const locationName = 'Sluisstraat';
+    const nextPage = 'sluisstraat';
+    location.assign(
+      `../navigate/index.html?coordinates=${coordinates}&locationName=${locationName}&nextPage=${nextPage}`,
+    );
+  };
